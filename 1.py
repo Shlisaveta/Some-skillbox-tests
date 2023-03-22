ea0823a4c046dc500b9aa63e7f313ef1318570c3
shlisaveta <>
2023-03-22 20:40:35 +0000

    Initial commit

---
+++ 1.py
@@ -0,0 +1,10 @@
+a = int(input('Сколько денег на счету'))
+discount = 0
+if a >= 75000:
+  print('Списать со счета деньги')
+  if a <= 5000:
+    print('Cделана скидка')
+    discount = 1000
+price = a + discout 
+print('Курс успешно приобритен')
+
